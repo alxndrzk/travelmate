2b6da3c8d7573470927c0472e2f216fb14510c13
alxndrzk <alexanderdwiputragultom@gmail.com>
2024-06-12 12:36:10 +0700

    start create homepage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './Styles/index.css';
 import Register from './Pages/register.jsx';
 import Login from './Pages/Login.jsx';
+import Homepage from './Pages/homepage.jsx';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
---
+++ src/Pages/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/index.css';
+
+const Homepage = () => {
+
+    return(
+    <div>start</div>
+    )
+    
+}
+
+export default Homepage
